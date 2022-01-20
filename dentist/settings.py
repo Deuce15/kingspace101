fc09e2dd880afdc02534e13e41a7fe2a50f4e7d0
Deuce <isaiahnyamu@gmail.com>
2022-01-20 23:21:16 +0300

    Update settings.py
    Removing acc and pwd

--- dentist/settings.py
+++ dentist/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kingspacebuilders@gmail.com'
-EMAIL_HOST_PASSWORD = 'kingspace@2021'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
